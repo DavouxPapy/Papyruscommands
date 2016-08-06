ca8827495486a2d19625ed0fb6b203c1d5a0d2fa
David Stone <papyrus8712@gmail.com>
2016-08-06 21:40:30 +0100

    Explained the file with comments

--- heal.lua
+++ heal.lua
@@ -1,20 +1,21 @@
-local ply = FindMetaTable("Player")
-function ply:healOverTime()
-	local hp = self:Health()
+--Shared file--
+local ply = FindMetaTable("Player") --Finds the meta table "Player", so the function can be applied to all players
+function ply:healOverTime() --Metafunction name
+	local hp = self:Health() --Returns the health of the player. "self" means whichever player the function's being tested on
 	if hp >= 100 then
-		self:ChatPrint("You already have maximum health")
-	elseif hp <= 99 and hp > 0 then
-		PrintMessage(HUD_PRINTTALK, "Player: " .. self:Nick() .. " is being healed")
+		self:ChatPrint("You already have maximum health") --Messaged printed to player's chat box
+	elseif hp <= 99 and hp > 0 then --If the previous if statement isnt true then...
+		PrintMessage(HUD_PRINTTALK, "Player: " .. self:Nick() .. " is being healed") --Prints a message to all player's chat boxes
 		local healRemaining = 100 - hp
-		timer.Create("Healplayer", 1, healRemaining, function() 
-			self:SetHealth(self:Health() + 1) 
+		timer.Create("Healplayer", 1, healRemaining, function() --Function happens over time
+			self:SetHealth(self:Health() + 1) --Slowly heals the player
 		end)
-	else
+	else --Anything else
 		self:ChatPrint("You are dead!")
 	end
 end
-hook.Add("PlayerSay", "HealPly", function(ply, text)
-	if string.lower(text) == "!healme" then
-		ply:healOverTime()
+hook.Add("PlayerSay", "HealPly", function(ply, text) --Called whenver the player types something
+	if string.lower(text) == "!healme" then --If the text the player writes is equal to "!healme"
+		ply:healOverTime() --Carries out the meta function on the player
 	end
 end)
